04d2008d7772ecb5879a720f340f3c6256cb1617
estherzzerpa <esterzerpa201@gmail.com>
2022-05-18 15:39:07 -0300

    destination

---
+++ app.js
@@ -0,0 +1,12 @@
+// const bgHome = document.querySelector(".bg-home")
+// const homeMenu = document.querySelector("#home")
+// const destinationMenu = document.querySelector("#destination").addEventListener("click", bgChange)
+// const crewMenu = document.querySelector("#crew")
+// const techMenu= document.querySelector("#tech")
+
+// const button = document.querySelector(".button_explore")
+
+
+// function bgChange(){
+
+// }
